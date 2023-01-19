c9493d426379a885f64af132a1fb56c6f3c6e19f
JValenueva <rykerluno@gmail.com>
2023-01-18 18:38:30 -0700

    created methods for robot class

--- robot.py
+++ robot.py
@@ -1,5 +1,8 @@
 class robot:
-    def __init__(self) -> None:
-        self.name = 
-        self.health = 
-        self.active_weapon = 
+    def __init__(self, name, Weapon) -> None:
+        self.name = name
+        self.health = '100'
+        self.active_weapon = Weapon
+
+    def attack(self, dinosaur):
+        self.dinosaur = []
